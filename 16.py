7ef4c3d00dd204e445c85e8fc033b00cdc143f4b
Akhil Chowdary dodda <imakhildodda17@gmail.com>
2021-04-25 22:24:00 +0530

    using seperator and end in print function

--- 16.py
+++ 16.py
@@ -1,2 +1,11 @@
 a, b, c, d, *e, f, g = range(20)
 print(len(e))
+
+for i in range(10):
+  try:
+    if 10 / i == 2.0:
+      break
+  except ZeroDivisionError:
+    print(1)
+  else:
+    print(2)
---
+++ C.py
@@ -0,0 +1,11 @@
+n=int(input())
+if(n==0):
+    print("it's a zero")
+elif(n=="String"):
+    print("it's a string")
+elif(n==0.0):
+    print("it's a floating value")
+elif(n=='5i+7'):
+    print("Complex")
+else:
+    print("Real")
